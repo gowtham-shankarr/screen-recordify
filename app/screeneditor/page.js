9e290e2b8e776db755a77bebd0aa23302d9acc3a
Gowtham shankar <gowthamshankar93@outlook.com>
2024-03-15 22:27:51 +0530

    screen after design

--- app/screeneditor/page.js
+++ app/screeneditor/page.js
@@ -1,10 +1,13 @@
-"use client"
-import React, { useState } from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
-const Layout = ({ children }) => {
-
-    const [canvasColor, setCanvasColor] = useState("bg-gradient-to-r from-red-500 to-orange-500")
+const ScreenEditor = ({ recordedVideoURL, children }) => {
+  const canvasRef = useRef(null);
+  const [canvasColor, setCanvasColor] = useState(
+    "bg-gradient-to-r from-red-500 to-orange-500"
+  );
 
   const bgColors = [
     "bg-gradient-to-r from-red-500 to-orange-500",
@@ -34,32 +37,103 @@ const Layout = ({ children }) => {
     "bg-gradient-to-r from-neutral-300 to-stone-400",
     "bg-gradient-to-r from-stone-500 to-stone-700",
     "bg-gradient-to-r from-slate-300 to-slate-500",
-    "bg-gradient-to-r from-emerald-500 to-emerald-900"
+    "bg-gradient-to-r from-emerald-500 to-emerald-900",
   ];
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    const video = document.createElement("video");
+
+    video.src = recordedVideoURL;
+    video.addEventListener("loadedmetadata", () => {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      video.play();
+    });
+
+    return () => {
+      video.pause();
+    };
+  }, [recordedVideoURL]);
 
   const handleColorClick = (color) => {
     setCanvasColor(color);
   };
 
+  const handleExportVideo = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+  
+    // Create a temporary canvas to draw the background color
+    const tempCanvas = document.createElement("canvas");
+    const tempCtx = tempCanvas.getContext("2d");
+  
+    // Set dimensions for temporary canvas
+    tempCanvas.width = canvas.width;
+    tempCanvas.height = canvas.height;
+  
+    // Draw the background color on the temporary canvas
+    tempCtx.fillStyle = canvasColor;
+    tempCtx.fillRect(0, 0, tempCanvas.width, tempCanvas.height);
+  
+    // Create a new video element
+    const video = document.createElement("video");
+    video.src = recordedVideoURL;
+  
+    // Wait for the video to load its metadata
+    video.onloadedmetadata = () => {
+      // Set canvas dimensions
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+
+      ctx.drawImage(tempCanvas, 0, 0, canvas.width, canvas.height);
+  
+      // Draw video frames onto canvas
+      const drawFrame = () => {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(drawFrame);
+      };
+  
+      // Start drawing frames
+      drawFrame();
+  
+      // Convert the canvas to a data URL
+      const image = canvas.toDataURL("image/png");
+  
+      // Create a download link for the image
+      const link = document.createElement("a");
+      link.download = "screen-recording.png";
+      link.href = image;
+      link.click();
+    };
+  };
+  
+  
+  
+
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-screen">
       <header className="bg-gray-800 text-white p-4 editor-header">
-      <div className="flex justify-between w-100">
-      <Image src="/screen-recordify-logo.svg" alt="Vercel Logo" width={150} height={100} />
-      <div>
-        <button className="export-btn">Export video</button>
-      </div>
-      </div>
+        <div className="flex justify-between w-100">
+          <Image
+            src="/screen-recordify-logo.svg"
+            alt="Vercel Logo"
+            width={150}
+            height={100}
+          />
+          <div>
+            <button className="export-btn" onClick={handleExportVideo}>Export video</button>
+          </div>
+        </div>
       </header>
       <main className="flex flex-1">
-        <div className="flex-1 bg-gray-200 p-8 editor-main">{children}
-        <div className="canvas-container">
-        <canvas className={`canvas-bg ${canvasColor} canvas-size`}></canvas>
-        </div>
-        
-        <div>
-            tes
-        </div>
+        <div className="flex-1 bg-gray-200 p-8 editor-main relative">
+          <div className="canvas-container relative">
+          {children}
+            <canvas className={`canvas-bg ${canvasColor} canvas-size`} ref={canvasRef}></canvas>
+          </div>
         </div>
         <aside className="editor-sidebar bg-gray-300 p-4">
           <div className="editor-right-blocks">
@@ -67,7 +141,8 @@ const Layout = ({ children }) => {
             <div className="flex flex-wrap pt-3">
               {bgColors.map((bgColor, index) => (
                 <div
-                  key={index} onClick={() => handleColorClick(bgColor)}
+                  key={index}
+                  onClick={() => handleColorClick(bgColor)}
                   className={`w-8 h-8 m-2 rounded-lg shadow-md ${bgColor}`}
                 ></div>
               ))}
@@ -76,30 +151,22 @@ const Layout = ({ children }) => {
 
           <div className="editor-right-blocks pt-5">
             <div className="text-sm text-white ">Padding</div>
-            <div className="pt-3">
-              Rang slider
-            </div>
+            <div className="pt-3">Rang slider</div>
           </div>
 
           <div className="editor-right-blocks pt-5">
             <div className="text-sm text-white ">Shadow</div>
-            <div className="pt-3">
-              Rang slider
-            </div>
+            <div className="pt-3">Rang slider</div>
           </div>
 
           <div className="editor-right-blocks pt-5">
             <div className="text-sm text-white ">Border Radius</div>
-            <div className="pt-3">
-              Rang slider
-            </div>
+            <div className="pt-3">Rang slider</div>
           </div>
-
-
         </aside>
       </main>
     </div>
   );
 };
 
-export default Layout;
+export default ScreenEditor;
--- app/screenrecorder/components/startingrecordedscreen.js
+++ app/screenrecorder/components/startingrecordedscreen.js
@@ -1,50 +1,203 @@
+"use client"
+import React, { useState, useRef, useEffect } from "react";
+import ScreenEditor from "../../screeneditor/page.js";
+import { FaPlay, FaPause, FaForward, FaBackward } from "react-icons/fa";
+
 export default function StartingRecordScreen() {
+  const [recording, setRecording] = useState(false);
+  const [screenActive, setScreenActive] = useState(false);
+  const [recordedVideoURL, setRecordedVideoURL] = useState("");
+  const recorderRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [videoDuration, setVideoDuration] = useState(0);
+
+  useEffect(() => {
+    const video = document.getElementById("videoPlayer");
+    if (video) {
+      video.onloadedmetadata = () => {
+        setVideoDuration(video.duration);
+      };
+    }
+  }, [recordedVideoURL]);
+
+  const startRecording = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: { mediaSource: "screen" },
+      });
+      recorderRef.current = new MediaRecorder(stream);
+      const chunks = [];
+
+      recorderRef.current.ondataavailable = (event) => {
+        chunks.push(event.data);
+      };
+
+      recorderRef.current.onstop = () => {
+        const blob = new Blob(chunks, { type: "video/webm" });
+        const videoURL = URL.createObjectURL(blob);
+        setRecordedVideoURL(videoURL);
+        setRecording(false);
+        setScreenActive(false);
+      };
+
+      recorderRef.current.start();
+      setRecording(true);
+    } catch (error) {
+      console.error("Error starting screen recording:", error);
+    }
+  };
+
+  const stopRecording = () => {
+    if (
+      recorderRef.current &&
+      recorderRef.current.state !== "inactive"
+    ) {
+      recorderRef.current.stop();
+    }
+  };
+
+  const handlePlayPause = () => {
+    const video = document.getElementById("videoPlayer");
+    if (video.paused) {
+      video.play();
+      setIsPlaying(true);
+    } else {
+      video.pause();
+      setIsPlaying(false);
+    }
+  };
+
+  const handleForward = () => {
+    const video = document.getElementById("videoPlayer");
+    video.currentTime += 10; 
+    setCurrentTime(video.currentTime);
+  };
+
+  const handleBackward = () => {
+    const video = document.getElementById("videoPlayer");
+    video.currentTime -= 10; 
+    setCurrentTime(video.currentTime);
+  };
+
+  const handleSeek = (event) => {
+    const video = document.getElementById("videoPlayer");
+    const seekTime = event.target.value;
+    video.currentTime = seekTime;
+    setCurrentTime(seekTime);
+  };
+
+  const handleClickScreen = () => {
+    setScreenActive(true);
+  };
 
   return (
     <div className="flex justify-center items-center h-screen flex-col">
-      <div className="text-center mb-6 max-w-[293px] p-0 sm:mb-8 sm:max-w-[370px] md:max-w-none md:mb-[50px]">
-        <div className="bg-gradient-to-r from-purple-600 to-purple-400 bg-clip-text text-transparent text-5xl font-semibold leading-12">
-          Capture Your Screen for Free
-        </div>
-        <div className="text-lg pt-2  font-normal">
-          Quickly and effortlessly record professional-quality videos
-        </div>
-      </div>
-      <div className="flex justify-center">
-
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <div className="py-4">
+       {!recording && !recordedVideoURL && (
+        <div>
+          <div className="text-center mb-6 max-w-[293px] p-0 sm:mb-8 sm:max-w-[370px] md:max-w-none md:mb-[50px]">
+            <div className="bg-gradient-to-r from-purple-600 to-purple-400 bg-clip-text text-transparent text-5xl font-semibold leading-12">
+              Capture Your Screen for Free
+            </div>
+            <div className="text-lg pt-2 font-normal">
+              Quickly and effortlessly record professional-quality videos
+            </div>
+          </div>
+          <div className="flex justify-center">
             <div
-              className="flex h-40 w-52 flex-col items-center justify-center rounded-2xl md:h-52 md:w-56 card-bg"
+              className={`max-w-sm rounded overflow-hidden shadow-lg ${
+                screenActive ? "cursor-pointer" : "cursor-not-allowed"
+              }`}
+              onClick={handleClickScreen}
             >
-              <div className="[&_svg]:transform-['scale(0.88)'] md:[&_svg]:transform-['scale(1)'] m-0 w-auto p-0 text-center sm:m-auto flex items-center flex-col">
-              <img src="./screen.svg" width={100} height={100}/>
-                <div className="pt-2.5 text-sm">Screen</div>
+              <div className="py-4">
+                <div
+                  className={`flex h-40 w-52 flex-col items-center justify-center rounded-2xl md:h-52 md:w-56 card-bg ${
+                    screenActive ? "bg-blue-200" : ""
+                  }`}
+                >
+                  <div className="[&_svg]:transform-['scale(0.88)'] md:[&_svg]:transform-['scale(1)'] m-0 w-auto p-0 text-center sm:m-auto flex items-center flex-col">
+                    <img src="./screen.svg" width={100} height={100} />
+                    <div className="pt-2.5 text-sm">Screen</div>
+                  </div>
+                </div>
               </div>
+            </div>
+
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="px-6 py-4">
+                <div className="flex h-40 w-52 flex-col items-center justify-center rounded-2xl md:h-52 md:w-56 card-bg">
+                  <div className="[&_svg]:transform-['scale(0.88)'] md:[&_svg]:transform-['scale(1)'] m-0 w-auto p-0 text-center sm:m-auto flex items-center flex-col">
+                    <img
+                      src="./camera-and-screen.svg"
+                      width={100}
+                      height={100}
+                    />
+                    <div className="pt-2.5 text-sm">Screen & Camera</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center">
+              <button
+                className={`mt-4 flex h-[72px] w-64 items-center justify-around gap-6 rounded-2xl ${
+                  recording ? "bg-red-600" : "bg-green-600"
+                } text-white hover:bg-red-700 active:bg-red-800 md:mt-1`}
+                onClick={recording ? stopRecording : startRecording}
+                disabled={!screenActive}
+              >
+                <div className="ml-3 text-sm">
+                  {recording ? "Stop Recording" : "Start Recording"}
+                </div>
+                <div className="flex h-12 w-12 content-center items-center rounded-2xl bg-white shadow-md">
+                  <div
+                    className={`mx-auto h-4 w-4 rounded-full bg-${
+                      recording ? "red" : "green"
+                    }-600 p-0`}
+                  ></div>
+                </div>
+              </button>
             </div>
           </div>
         </div>
-
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <div className="px-6 py-4">
-            <div className="flex h-40 w-52 flex-col items-center justify-center rounded-2xl md:h-52 md:w-56 card-bg">
-              <div className="[&_svg]:transform-['scale(0.88)'] md:[&_svg]:transform-['scale(1)'] m-0 w-auto p-0 text-center sm:m-auto flex items-center flex-col">
-                <img src="./camera-and-screen.svg" width={100} height={100}/>
-                <div className="pt-2.5 text-sm">Screen & Camera</div>
-              </div>
+      )}
+      {!recording && recordedVideoURL && (
+        <div className="mt-4 relative w-full">
+          <ScreenEditor recordedVideoURL={recordedVideoURL}> 
+            <video
+              id="videoPlayer"
+              className="video-render-screen"
+              width="500"
+              height="250"
+              onTimeUpdate={(e) => setCurrentTime(e.target.currentTime)}
+            >
+              <source src={recordedVideoURL} type="video/mp4" />
+            </video>
+            <div className="player-controls flex items-center justify-center">
+              <div className="mx-8">{formatTime(currentTime)}</div>
+              <button onClick={handleBackward}>
+                <FaBackward className="text-2xl"/>
+              </button>
+              <button onClick={handlePlayPause} className="mx-8">
+                {isPlaying ? <FaPause className="text-2xl" /> : <FaPlay className="text-2xl" />}
+              </button>
+              <button onClick={handleForward}>
+                <FaForward className="text-2xl"/>
+              </button>
+              {videoDuration > 0 && (
+                    <div className="mx-8">{formatTime(videoDuration)}</div>
+                  )}
             </div>
-          </div>
+          </ScreenEditor>
         </div>
-
-        <div className="flex flex-col items-center justify-center">
-          <button className="mt-4 flex h-[72px] w-64 items-center justify-around gap-6 rounded-2xl bg-red-600 text-white hover:bg-red-700 active:bg-red-800 md:mt-1">
-            <div className="ml-3 text-sm">Start Recording</div>
-            <div className="flex h-12 w-12 content-center items-center rounded-2xl bg-white shadow-md">
-              <div className="mx-auto h-4 w-4 rounded-full bg-red-600 p-0"></div>
-            </div>
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
+}
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
